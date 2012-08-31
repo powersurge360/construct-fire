4f26ab0baa0050f2748e214aebaffeb9b80f8b41
Kevin Diale <powersurge360@gmail.com>
2012-08-30 22:59:17 -0500

    Moved the entry template to a more default place.

--- fireblog/views.py
+++ fireblog/views.py
@@ -4,7 +4,6 @@ from fireblog.models import Entry
 
 
 class EntryListView(ListView):
-    template_name = "fireblog/entries.html"
     queryset = Entry.objects.filter(published="p")
     context_object_name = "entries"
     paginate_by = 10
