4e4e2f20bffb4c64aae16caf520a9b36c294d7b7
Kevin Diale <powersurge360@gmail.com>
2012-09-15 15:27:22 -0500

    Added an action for publishing multiple entries at once and fixed the index page.

--- fireblog/admin.py
+++ fireblog/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from fireblog.models import Entry
 
 
+def publish_selected(modeladmin, request, queryset):
+    queryset.update(published="p")
+
+publish_selected.short_description = "Publish selected Entries"
+
+
 class EntryAdmin(admin.ModelAdmin):
     exclude = ("slug", "markup", "author",)
     list_display = ("title", "date", "published",)
+    actions = [publish_selected]
 
     def save_model(self, request, obj, form, change):
         obj.author = request.user
